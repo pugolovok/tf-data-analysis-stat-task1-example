c1efb1a21b4a5d3fafba2832e5afbbf0eb68e8a6
pugolovok <73109410+pugolovok@users.noreply.github.com>
2023-04-08 02:36:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 682912115 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
+    a = np.zeros(x.shape[0])
+    for i in a:
+        a[i] = x[i]*2/70**2
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return a.mean() # Ваш ответ
